2fab754b7073bceee3c55684e7315c12a9f85aba
nithishmedi <134866152+nithishmedi@users.noreply.github.com>
2026-06-18 17:26:27 +0530

    Update server.js

--- server.js
+++ server.js
@@ -6,10 +6,52 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("Critical AI Server Running");
+app.post("/chat", async (req, res) => {
+  try {
+    const message = req.body.message;
+
+    const response = await fetch(
+      `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent?key=${process.env.GEMINI_API_KEY}`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+          contents: [
+            {
+              parts: [
+                {
+                  text: message
+                }
+              ]
+            }
+          ]
+        })
+      }
+    );
+
+    const data = await response.json();
+
+    const reply =
+      data.candidates?.[0]?.content?.parts?.[0]?.text ||
+      "No response";
+
+    res.json({ reply });
+
+  } catch (error) {
+    res.json({
+      reply: "Error connecting to Gemini"
+    });
+  }
 });
 
-app.listen(3000, () => {
+app.get("/", (req, res) => {
+  res.send("Critical AI Running");
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
   console.log("Server Started");
 });
